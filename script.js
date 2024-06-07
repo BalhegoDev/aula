05fa530a0aa1a7965d6626d29e4a25ca9bb4a4af
Bros12Lek <adgamesbalhego@hotmail.com>
2024-06-07 16:35:30 -0300

    Classe veiculo adicionada

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Aula de git");
+class Carro{
+    constructor(tipo,capacidade){
+        this.tipo = tipo;
+        this.capacidade = capacidade
+    }
+}
+
+const gol = new Carro("carro",5);
