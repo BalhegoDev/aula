ae98213470e2bbf6b0c571ec5e1901cddee8c661
Bros12Lek <adgamesbalhego@hotmail.com>
2024-06-07 17:28:01 -0300

    classe animal adicionada

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("branch suporte");
+console.log("Aula de git");
+class Carro{
+    constructor(tipo,capacidade){
+        this.tipo = tipo;
+        this.capacidade = capacidade
+    }
+}
+
+const gol = new Carro("carro",5);
+
+class Animal{
+  constructor(tipo,patas){
+    this.tipo = tipo;
+    this.patas = patas;
+  }   
+}
+
+const cachorro = new Animal("mamífero",4);
